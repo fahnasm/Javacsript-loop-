062c58278ad3b507d4af558bb431848054ffefc0
fahnasm <fahna.sabilla11@gmail.com>
2023-02-23 10:58:42 +0700

    new

---
+++ script.js
@@ -0,0 +1,19 @@
+// while(noAngkot <= jmlAngkot){
+//     console.log('Angkot No. '+ noAngkot + ' beroperasi dengan baik');
+// noAngkot++;
+// }
+
+var noAngkot = 1;
+var jmlAngkot = 10;
+var angkotBeroperasi = 6;
+
+while(noAngkot <= angkotBeroperasi){
+    console.log('Angkot No. ' + noAngkot + ' beroperasi dengan baik');
+noAngkot++;
+} 
+for ( noAngkot >= angkotBeroperasi; noAngkot <= jmlAngkot; noAngkot++){
+    console.log('Angkot No. ' + noAngkot + ' sedang tidak beroperasi');
+}
+
+
+
